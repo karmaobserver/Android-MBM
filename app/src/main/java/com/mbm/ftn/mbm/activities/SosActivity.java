4b2e17144b56098771c49ad7ed0221ccfa8f1785
MIKO\Milos <milosvujosevic@hotmail.com>
2017-06-17 05:11:53 +0200

    Map in SosActivity

--- app/src/main/java/com/mbm/ftn/mbm/activities/SosActivity.java
+++ app/src/main/java/com/mbm/ftn/mbm/activities/SosActivity.java
@@ -1,14 +1,16 @@
 package com.mbm.ftn.mbm.activities;
 
-import android.Manifest;
 import android.content.Intent;
-
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
-
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.mbm.ftn.mbm.R;
 
 
@@ -16,7 +18,9 @@ import com.mbm.ftn.mbm.R;
  * Created by Makso on 6/17/2017.
  */
 
-public class SosActivity extends BaseActivity  {
+public class SosActivity extends BaseActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +32,10 @@ public class SosActivity extends BaseActivity  {
 
         getSupportActionBar().setLogo(R.mipmap.ic_sos);
         getSupportActionBar().setDisplayUseLogoEnabled(true);
+
+        SupportMapFragment mapFragment =
+                (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
 
 
     }
@@ -62,6 +70,11 @@ public class SosActivity extends BaseActivity  {
 
         }
 
+    }
+
+    @Override
+    public void onMapReady(GoogleMap map) {
+        map.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
     }
 
 
